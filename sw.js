c09686f34fef5bb593882abd2d87b54d8730e267
Hassan Mahmud <39611452+hms6395@users.noreply.github.com>
2026-07-08 16:28:01 +0600

    Update cache version from v1 to v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'khata-v1';
+const CACHE = 'khata-v2';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e)=>{
